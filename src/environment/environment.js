dc24ec5cfcae1797e59db776715c372f879152ea
kkhtun <khaingkhanthtun@gmail.com>
2022-08-03 21:44:38 +0630

    temporarily use NODE_ENV for host differentiation between dev and prod

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,4 +1,4 @@
 export const HOST =
-    process.env.REACT_APP_ENV === "development"
+    process.env.NODE_ENV === "production"
         ? "https://chess-dev.herokuapp.com"
         : "http://localhost:8080";
