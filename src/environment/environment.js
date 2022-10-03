cd4067f2ae13b5467e16bc6f9e6e170fa9879da4
kkhtun <khaingkhanthtun@gmail.com>
2022-10-03 16:17:35 +0630

    changed host to render.com

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,4 +1,4 @@
 export const HOST =
     process.env.NODE_ENV === "production"
-        ? "https://chess-dev.herokuapp.com"
+        ? "https://coffee-chess-server.onrender.com"
         : "http://localhost:8080";
